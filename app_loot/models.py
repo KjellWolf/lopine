11459b68b226d3b4903b3c51c04f5426f42409c8
KjellWolf <g.schneider@carnivore-wolfs.net>
2023-11-01 13:59:19 +0100

    still testing

--- app_loot/models.py
+++ app_loot/models.py
@@ -50,7 +50,7 @@ class Item(models.Model):
     )
 
     item_id = models.AutoField(primary_key=True)
-    rulesystem = models.CharField(max_length=20, choices=SYSTEM_CHOICES, default='')
+    rulesystem = models.CharField(verbose_name='Rulesystem (*)', max_length=20, choices=SYSTEM_CHOICES, default='')
     language = models.CharField(max_length=20, choices=LANG_CHOICES, default='')
     item_type = models.CharField(max_length=20, choices=ITEM_TYP_CHOICES, default='Weapon')
     weight_percentage = models.IntegerField(default=500, validators=[MaxValueValidator(950)])
