05a7307600c37689f3e7f9dcee49bbd603b85c0a
gabriel.schneider <gabriel.schneider@lumaserv.com>
2023-11-09 15:35:41 +0100

    Polls Update

--- lopine_items_app/urls.py
+++ lopine_items_app/urls.py
@@ -1,2 +1,9 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from . import views
+
 urlpatterns = [
+    path('lootgen.html', views.show_lootgen, name='lootgen.html'),
 ]
--- lopine_items_app/views.py
+++ lopine_items_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def show_lootgen(request):
+    return render(request, 'lootgen.html')
