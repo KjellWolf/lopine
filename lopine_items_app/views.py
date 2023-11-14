607a953c4a11536cd6fec0a3cfc96a01df9780a1
gabriel.schneider <gabriel.schneider@lumaserv.com>
2023-11-14 18:01:23 +0100

    frondend tests

--- lopine_items_app/views.py
+++ lopine_items_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def show_lootgen(request):
-    return render(request, 'lootgen.html')
+    return render(request, 'lopine_items_app/lootgen.html')
